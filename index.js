0e14bfc86c7ddc3fdd0c08eeba37975f227f7280
RevX <127177436+revx0012@users.noreply.github.com>
2023-09-16 21:51:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, IntentsBitField, MessageMentions } = require('discord.js');
+const { Client, IntentsBitField, Permissions } = require('discord.js');
 const { GradioChatBot } = require('gradio-chatbot');
 const fs = require('fs');
 const channelsFile = 'channels.json';
@@ -30,18 +30,18 @@ function loadServerSettings() {
     }
 }
 
-function saveServerSettings() {
-    fs.writeFileSync(channelsFile, JSON.stringify(serverSettings, null, 4), 'utf8');
-}
-
 function loadRules() {
     try {
         const data = fs.readFileSync(rulesFile, 'utf8');
-        return data.split('\n').filter((rule) => rule.trim() !== '');
+        return data.split('\n');
     } catch (error) {
         console.error('Error loading rules:', error);
         return [];
     }
+}
+
+function saveServerSettings() {
+    fs.writeFileSync(channelsFile, JSON.stringify(serverSettings, null, 4), 'utf8');
 }
 
 function saveRules() {
@@ -52,13 +52,13 @@ client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-async function createBot(rules) {
+async function createBot(rulesText) {
     if (!botInstance) {
         botInstance = new GradioChatBot({
             url: 'https://huggingface.co/spaces/mosaicml/mpt-30b-chat'
         });
         console.log("Setting system prompt..");
-        await botInstance.chat(`Hello! You are a chatbot named "Chatbot". ${rules}`);
+        await botInstance.chat(`Hello! You are a chatbot named "Chatbot". ${rulesText}`);
     }
 
     return {
@@ -75,16 +75,21 @@ client.on('messageCreate', async (message) => {
     const serverId = message.guild.id;
     const splitMessage = message.content.toLowerCase().split(' ');
 
+    // Check if the message starts with one of the allowed commands
+    const allowedCommands = ['restart', 'chatbot', 'ruleadd']; // Add other allowed commands here
+    if (allowedCommands.includes(splitMessage[0])) {
+        // Check if the user has BAN_MEMBERS permission for commands
+        if (!message.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS)) {
+            message.channel.send('You do not have permission to use this command.');
+            return;
+        }
+    }
+
     if (splitMessage[0] === PREFIX.toLowerCase()) {
         const command = splitMessage[1];
 
         if (command === 'chatbot') {
-            // Check if the user has BAN_MEMBERS permission
-            if (!message.member.permissions.has('BAN_MEMBERS')) {
-                message.channel.send('You do not have permission to use this command.');
-                return;
-            }
-
+            // The user wants to set up the chatbot for this channel
             if (splitMessage[2] && splitMessage[2].startsWith('<#') && splitMessage[2].endsWith('>')) {
                 const channelId = splitMessage[2].slice(2, -1);
 
@@ -104,30 +109,14 @@ client.on('messageCreate', async (message) => {
                 message.channel.send('Please specify a valid channel to set up the chatbot.');
             }
         } else if (command === 'restart') {
-            // Check if the user has BAN_MEMBERS permission
-            if (!message.member.permissions.has('BAN_MEMBERS')) {
-                message.channel.send('You do not have permission to use this command.');
-                return;
-            }
-
-            message.channel.send('Restarting...').then(() => {
-                process.exit(0); // Replace this with your bot's restart logic
-            });
+            botInstance = null;
+            message.channel.send('Bot has been restarted.');
         } else if (command === 'ruleadd') {
-            // Check if the user has BAN_MEMBERS permission
-            if (!message.member.permissions.has('BAN_MEMBERS')) {
-                message.channel.send('You do not have permission to use this command.');
-                return;
-            }
-
-            const newRule = message.content.substring(PREFIX.length + command.length + 1);
-
-            if (newRule.trim() !== '') {
-                rules.push(newRule);
-
+            if (splitMessage.length > 2) {
+                const newRule = message.content.substring(message.content.indexOf('"') + 1, message.content.lastIndexOf('"'));
+                rules.push(`@ChatGPT ${newRule}`);
                 saveRules();
-
-                message.channel.send(`Rule added: "${newRule}"\nUse the command \`restart\` to restart manually.`);
+                message.channel.send(`Rule added: "${newRule}"\nUse the command \`restart\` to apply changes.`);
             } else {
                 message.channel.send('Please provide a valid rule to add.');
             }
@@ -135,7 +124,7 @@ client.on('messageCreate', async (message) => {
     }
 
     if (serverSettings[serverId] && serverSettings[serverId].channelId === message.channel.id) {
-        const bot = await createBot(rules);
+        const bot = await createBot(rules.join('\n'));
         message.channel.sendTyping();
         const response = await bot.send(message.content);
         message.channel.send(`[BOT]: ${response}`);
