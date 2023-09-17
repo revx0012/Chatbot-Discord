ecf539fb3bb153adab429ff562ceb1212f7c9553
RevX <127177436+revx0012@users.noreply.github.com>
2023-09-17 09:42:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ const TOKEN = process.env['TOKEN'];
 const PREFIX = '<@1141993367169941504>';
 
 let serverSettings = loadServerSettings();
-let botInstance = null;
 
 function loadServerSettings() {
     try {
@@ -49,12 +48,13 @@ client.on('messageCreate', async (message) => {
     if (splitMessage[0] === PREFIX.toLowerCase()) {
         const command = splitMessage[1];
 
-        if (command === 'chatbot' && message.member.permissions.has(Permissions.Flags.Ban_Members)) {
-                message.channel.send('You do not have permission to use this command.');
-                return;
-            }
+        if (command === 'chatbot' && !message.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS)) {
+            message.channel.send('You do not have permission to use this command.');
+            return;
+        }
 
-            // The user wants to set up the chatbot for this channel
+        // The user wants to set up the chatbot for this channel
+        if (command === 'chatbot') {
             if (splitMessage[2] && splitMessage[2].startsWith('<#') && splitMessage[2].endsWith('>')) {
                 const channelId = splitMessage[2].slice(2, -1);
 
@@ -73,24 +73,14 @@ client.on('messageCreate', async (message) => {
             } else {
                 message.channel.send('Please specify a valid channel to set up the chatbot.');
             }
-        } else if (command === 'restart') {
-            // Check if the user has ban permission
-            if (!message.member.permissions.has(Permissions.Flags.Ban_Members)) {
-                message.channel.send('You do not have permission to use this command.');
-                return;
-            }
-
+        } else if (command === 'restart' && !message.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS)) {
+            message.channel.send('You do not have permission to use this command.');
+        } else if (command === 'restart' && message.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS)) {
             message.channel.send('Restarting...');
             process.exit();
-            client.login(TOKEN)
-        } else if (command === 'ruleadd') {
-            // Check if the user has ban permission
-            if (!message.member.permissions.has(Permissions.Flags.Ban_Members)) {
-                message.channel.send('You do not have permission to use this command.');
-                return;
-            }
-
-            
+        } else if (command === 'ruleadd' && !message.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS)) {
+            message.channel.send('You do not have permission to use this command.');
+        } else if (command === 'ruleadd' && message.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS)) {
             const newRule = message.content.slice(command.length + PREFIX.length + 1); // Extract the rule text
 
             // Check if the ruleToAdd is not empty
